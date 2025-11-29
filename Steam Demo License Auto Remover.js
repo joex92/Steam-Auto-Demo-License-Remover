692b0736aaa0a05f0f3fbd749038100aee6a6695
Joe X! <jamedinao@gmail.com>
2025-11-29 15:10:22 -0500

    Update Steam Demo License Auto Remover.js
    Signed-off-by: Joe X! <jamedinao@gmail.com>

--- Steam Demo License Auto Remover.js
+++ Steam Demo License Auto Remover.js
@@ -52,7 +52,7 @@
         console.log(`[${storageKey}] Old size: ${currentList.length} | New size: ${updatedList.length}`);
 
         // 4. SAVE: Stringify and save back to storage
-        return await GM.setValue(storageKey, JSON.stringify(newItemsArray));
+        return await GM.setValue(storageKey, JSON.stringify(updatedList));
     }
     
     class SleepTimer {
