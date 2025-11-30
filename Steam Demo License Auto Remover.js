a6329f297fedefb5e6fd523959408a4d0d328722
Joe X! <jamedinao@gmail.com>
2025-11-29 19:23:54 -0500

    Update Steam Demo License Auto Remover.js
    Signed-off-by: Joe X! <jamedinao@gmail.com>

--- Steam Demo License Auto Remover.js
+++ Steam Demo License Auto Remover.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         One-Click Steam Demo License Auto Remover
 // @namespace    https://github.com/joex92/Steam-Auto-Demo-License-Remover
-// @version      6.0
+// @version      6.1
 // @description  Original by PeiqiLi. This is an English Translated version with the addition of removing demo/prologue titles only.
 // @author       PeiqiLi + JoeX92
 // @match        https://store.steampowered.com/account/licenses/
