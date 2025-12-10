673cbbdbc0130b8b4c2b4adf291c664b00141880
Joe X! <jamedinao@gmail.com>
2025-12-10 09:52:13 -0500

    Update Steam Demo License Auto Remover.js
    Signed-off-by: Joe X! <jamedinao@gmail.com>

--- Steam Demo License Auto Remover.js
+++ Steam Demo License Auto Remover.js
@@ -28,7 +28,7 @@
     const demoWesternKeywords = [// "ost", "soundtrack",
         "free weekend", "demo", "demover", "trial", "episode", "chapter",
         "alpha", "beta", "sample", "part", "trailer", "playtest", "prolog[a-z]*",
-        "preview", "benchmark", "teaser", "spectator", "early access",
+        "preview", "benchmark", "teaser", "spectator", "early access", "prolouge",
         // Russian
         "демо", "пролог", "эпизод", "альфа", "бета", "тест",
         "пробная", "тизер", "плейтест", "ознакомительная"
